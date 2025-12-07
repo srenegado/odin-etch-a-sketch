ae161cdd818eb95e8895c1edfc4c266f18ca8e30
srenegado <scottstationary@gmail.com>
2025-12-06 20:35:01 -0800

    Add 16x16 grid of squares

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
-function createSketchpadSquare() {
+function createSketchpad() {
   const sketchpad = document.querySelector(".sketchpad");
-  const square = document.createElement("div");
-  
-  square.classList.add("blank-square");
-  square.addEventListener("mouseenter", (e) => {
-    e.target.classList.add("coloured-square");
-    e.target.classList.remove("blank-square");
-  })
-  
-  sketchpad.appendChild(square);
+  const numOfSquaresPerSide = 16;
+  const numOfSquares = Math.pow(numOfSquaresPerSide, 2);
+
+  for (let i = 0; i < numOfSquares; i++) {
+    const square = document.createElement("div");
+    
+    square.classList.add("blank-square");
+    square.addEventListener("mouseenter", (e) => {
+      e.target.classList.add("coloured-square");
+      e.target.classList.remove("blank-square");
+    })
+    
+    sketchpad.appendChild(square);
+  }
 }
 
-createSketchpadSquare();
+createSketchpad();
